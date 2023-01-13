e4902f0433e1a356126b8ab30943a8075b45eda4
Alex Reed <areed829@gmail.com>
2023-01-13 16:03:34 -0500

    wip: Work through tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+    let secret_number = 42;
+
+    println!("The secret number is: {}", secret_number);
+
+    // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    // println!("You guessed: {}", guess);
+
+    // if guess == secret_number {
+    //     println!("You win!");
+    // } else if guess < secret_number {
+    //     println!("Too small!");
+    // } else {
+    //     println!("Too big!");
+    // }
 }
